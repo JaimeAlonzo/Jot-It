6ac40515927bdd5ce82f8cf12fae2cf6b1ac1383
Jaime Zamudio <jaime.alonzo.97@hotmail.com>
2019-11-07 10:47:46 -0500

    Intial Commit

---
+++ public/javascript/home.js
@@ -0,0 +1,11 @@
+document.getElementById("lin").onclick = function() {
+    self.frames.iframe_a.location.href = 'https://www.linguee.es/';
+};
+
+document.getElementById("zip").onclick = function() {
+    self.frames.iframe_a.location.href = 'https://www.unitedstateszipcodes.org/';
+};
+
+document.getElementById("mer").onclick = function() {
+    self.frames.iframe_a.location.href = 'https://www.merriam-webster.com/';
+};
